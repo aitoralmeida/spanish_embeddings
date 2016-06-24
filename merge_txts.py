a1159e72e4ffb830c637f190d75bb0cc8d7c49f3
Aitor Almeida <aitoralmeida@gmail.com>
2016-06-24 09:44:44 +0200

    text merger

--- merge_txts.py
+++ merge_txts.py
@@ -5,7 +5,7 @@ Created on Thu Jun 23 14:27:07 2016
 @author: aitor
 """
 
-#USAGE: python merge_txts.py boe boe.text
+#USAGE: python merge_txts.py boe boe.text text
 
 import glob
 import string
@@ -18,15 +18,19 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         print globals()['__doc__'] % locals()
         sys.exit(1)
-    inp, outp = sys.argv[1:3]
+    inp, outp, extension = sys.argv[1:4]
     output = open(outp, 'w')
-    
+    print 'Input folder:', inp
+    print 'Outpuf file:', outp
     i = 0    
     MINIMUN_LENGTH = 20 # we do not process paragraphs with less than this chars     
     exclude = set(string.punctuation) 
     exclude.add(u'–')      
-    for book_file in glob.glob(inp + '/*.txt'):
+    files_path = inp + '/*.' + extension
+    print 'Starting file processing:' + files_path
+    for book_file in glob.glob(files_path):
         i = i + 1
+        print 'Processing:', book_file
         with open(book_file, 'r') as book:
             for line in book:
                 text = line.strip().lower()
